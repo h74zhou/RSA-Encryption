7d6df274f1fc324fefb915a5b8874570de8f6ace
Herun Zhou <iamherunzhou@gmail.com>
2020-02-17 14:14:56 -0500

    created RSA class

---
+++ src/RSA.java
@@ -0,0 +1,13 @@
+public class RSA {
+
+    private static int getPrivateKey(int n, int e) {
+        // Computes
+        
+    }
+
+
+    public static void main(String args[]) {
+        getPrivateKey(851, 631);
+
+    }
+}
